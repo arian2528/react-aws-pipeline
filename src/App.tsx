19fa3e6d8180b53661dbe844fba1e3000cfc5731
arian2528 <arian2528@gmail.com>
2023-11-02 15:08:47 -0400

    Removing default screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={'https://img.freepik.com/free-vector/hand-drawn-construction-background_23-2147734520.jpg?w=996&t=st=1698951595~exp=1698952195~hmac=0dd66dca64dfc0fc4e054e1289b7801caa040ed963d6dbc218daf5aad1063932'} className="App-logo" alt="logo" />
       </header>
     </div>
   );
