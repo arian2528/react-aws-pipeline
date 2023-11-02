b50dc9630835c86b1e097601d612c3d2065846ff
arian2528 <arian2528@gmail.com>
2023-11-02 15:44:18 -0400

    Fixing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Basic React app using Docker, Hook up to AWS CI/CD CodePipeline on S3 using GitHub Actions message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(`Basic React app using Docker, Hook up to AWS CI/CD CodePipeline on S3 using GitHub Actions`);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={'https://img.freepik.com/free-vector/hand-drawn-construction-background_23-2147734520.jpg?w=996&t=st=1698951595~exp=1698952195~hmac=0dd66dca64dfc0fc4e054e1289b7801caa040ed963d6dbc218daf5aad1063932'} className="App-logo" alt="logo" />
+        <p>Basic React app using Docker, Hook up to AWS CI/CD CodePipeline on S3 using GitHub Actions</p>
       </header>
     </div>
   );
