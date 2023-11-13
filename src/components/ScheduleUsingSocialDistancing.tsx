4f8967d5850eef8b80978fea107c9f54bac2aeb0
arian2528 <arian2528@gmail.com>
2023-11-13 18:08:07 -0500

    Schedule social distancing component

---
+++ src/components/ScheduleUsingSocialDistancing.tsx
@@ -0,0 +1,36 @@
+import { FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
+import React, { Fragment } from "react";
+
+interface ScheduleUsingSocialDistancingOptions {
+    value: string;
+    label: string;
+}
+
+const ScheduleUsingSocialDistancing = () => {
+
+    const scheduleUsingSocialDistancingOptions: ScheduleUsingSocialDistancingOptions[] = [
+        {value: 'true', label: 'Yes'},
+        {value: 'false', label: 'No'},
+    ];
+
+    const [scheduleUsingSocialDistancing, setScheduleUsingSocialDistancing] = React.useState<string>('false');
+
+    return (
+        <Fragment>
+            <FormLabel id="demo-row-radio-buttons-group-label">Split schedule using social distancing?</FormLabel>
+            <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                value={scheduleUsingSocialDistancing}
+                onChange={(event) => setScheduleUsingSocialDistancing(event.target.value)}
+            >
+                {scheduleUsingSocialDistancingOptions && scheduleUsingSocialDistancingOptions.map((option) => (
+                    <FormControlLabel key={option.value} value={option.value} control={<Radio />} label={option.label} />
+                ))}
+            </RadioGroup>
+        </Fragment>
+    )
+}
+
+export default ScheduleUsingSocialDistancing;
